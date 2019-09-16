1285cf79a642bd82a6f155f000095d46adb3e507
tewtew023 <38246059+tewtew023@users.noreply.github.com>
2019-09-17 01:58:36 +0700

    สาหมน้าดท่าเดิม

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 
-  document.addEventListener('init', function(event) {
-    var page = event.target;
+  // document.addEventListener('init', function(event) {
+  //   var page = event.target;
   
-    if (page.id === 'home') {
-      page.querySelector('#menu_button').onclick = function() {
-        document.querySelector('#menu').open();
-      };
+  //   if (page.id === 'home') {
+  //     page.querySelector('#menu_button').onclick = function() {
+  //       document.querySelector('#menu').open();
+  //     };
   
-      //The postchange event listener is used for changing the dots when the carousel is changed
+  //     //The postchange event listener is used for changing the dots when the carousel is changed
   
-      page.querySelector('#carousel').addEventListener("postchange", function() {
-        page.querySelector('#dot0').classList.remove("circle_current");
-        page.querySelector('#dot1').classList.remove("circle_current");
-        page.querySelector('#dot2').classList.remove("circle_current");
+  //     page.querySelector('#carousel').addEventListener("postchange", function() {
+  //       page.querySelector('#dot0').classList.remove("circle_current");
+  //       page.querySelector('#dot1').classList.remove("circle_current");
+  //       page.querySelector('#dot2').classList.remove("circle_current");
   
-        page.querySelector('#dot' + page.querySelector('#carousel').getActiveIndex()).classList.add("circle_current");
+  //       page.querySelector('#dot' + page.querySelector('#carousel').getActiveIndex()).classList.add("circle_current");
   
-      });
-    } else if (page.id === 'movie_details') {
-      page.querySelector('#movie_title').innerHTML = page.data.title;
-    }
-  });
+  //     });
+  //   } else if (page.id === 'movie_details') {
+  //     page.querySelector('#movie_title').innerHTML = page.data.title;
+  //   }
+  // });
   
   function openMovieDetails(id) {
     document.querySelector('#myNavigator').pushPage('movie_details.html', {data: {title: id}});
@@ -34,4 +34,10 @@
     document.querySelector('#menu').close().then(function() {
       document.querySelector('#myNavigator').popPage()
     });
+  }
+  function openLogin() {
+    document.querySelector('#myNavigator').pushPage('pagelogin.html');  
+  }
+  function openSignup() {
+    document.querySelector('#myNavigator').pushPage('pageRegister.html');  
   }
