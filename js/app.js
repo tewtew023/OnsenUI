a4b2a06905b6b7d18961b27f01072e89f87c8a0d
tewtew023 <38246059+tewtew023@users.noreply.github.com>
2019-10-15 21:01:26 +0700

    Google login

--- js/app.js
+++ js/app.js
@@ -49,8 +49,8 @@ function Paysuccess() {
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
-
-
+var provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
 
 document.addEventListener('init', function (event) {
@@ -113,6 +113,35 @@ document.addEventListener('init', function (event) {
 
       });
     });
+
+
+
+    $('#gglogin').click( function googleLogin() {
+      firebase.auth().signInWithRedirect(provider);
+      firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+        console.log('user'+token);
+        
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        console.log('error'+errorCode);
+      });
+    });
+   
+
 
   }
   //End Login page
